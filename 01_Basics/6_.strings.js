da214727cbf59f2c51e224ca7c82386adad5d39c
Shagun Sharma <159492419+ShagunSharma17@users.noreply.github.com>
2024-06-22 13:47:34 +0000

    Numbers and Math functions

--- 01_Basics/6_.strings.js
+++ 01_Basics/6_.strings.js
@@ -9,10 +9,13 @@ String interpolation--involves embedding expressions and we can change them on t
 whereas String formatting--concatenating strings and variables 
 using operators or methods. */
 
+
+
+
 console.log(`This repository is created by ${name}`);
 
-let repositoryName= new String('JAVASCRIPT');
-
+let repositoryName= new String('JAVA-SCRIPT');
+console.log(repositoryName);
 console.log(`The name of repository is ${repositoryName}`);
 /*
 If both the ways of defining a string are same then what is the difference
@@ -34,8 +37,37 @@ length: 10
 
 here, 0,1.. are not indexes but are key value pairs*/
 
+
+
+
 console.log(repositoryName[8]);
 console.log(repositoryName.__proto__);
 
 console.log(repositoryName.length);
-console.log(repositoryName.charAt(3));
+console.log(repositoryName.charAt(4));
+console.log(repositoryName.indexOf('J'));
+console.log(repositoryName.indexOf('B'));//doesn't exsits hence returns -1
+
+
+
+//substring in js
+let substrings= repositoryName.substring(0, 4) //prints 4-1
+console.log(substrings);
+
+//slice
+let sliceope= repositoryName.slice(-10, -3) //takes negative inputs
+console.log(sliceope);
+
+//split
+let places="rishikesh himachal himalayas odisa kerela"
+let spliteope= places.split('i') //divides string into array based on '-' and doesn't iclude ' '
+console.log(spliteope);
+
+
+let str='          hello world                '
+console.log(str);
+console.log(str.trim());
+console.log((str.trim()).replace('h','H'));
+
+
+console.log(Number(str.includes('she')));
---
+++ 01_Basics/7_Nums_and_Maths.js
@@ -0,0 +1,8 @@
+const score=202
+const balance= new Number(203)//treats as object
+console.log(score)
+console.log(balance);
+
+
+
+
