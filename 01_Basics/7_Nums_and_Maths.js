a4030ce4ea808173c1123bfc176a2c6df24c82ea
Shagun Sharma <159492419+ShagunSharma17@users.noreply.github.com>
2024-06-23 12:59:02 +0000

    container config

--- 01_Basics/7_Nums_and_Maths.js
+++ 01_Basics/7_Nums_and_Maths.js
@@ -3,6 +3,8 @@ const balance= new Number(203)//treats as object
 console.log(score)
 console.log(balance);
 
+console.log(typeof(balance.toString()));
+console.log(balance.toFixed(2));//digits after point, if toFixed() by default take 0
 
 
 
