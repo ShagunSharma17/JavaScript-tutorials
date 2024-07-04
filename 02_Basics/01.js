6d61a84626760b5b3a1efa805a50b32dcb954462
Shagun Sharma <159492419+ShagunSharma17@users.noreply.github.com>
2024-07-04 15:19:20 +0000

    Arrays

---
+++ 02_Basics/01.js
@@ -0,0 +1,31 @@
+const myArr=[1,2,3];
+console.log(myArr);
+
+const magazine= new Array("VOGUE", "BAZZAR", "FORBES")
+
+console.log(myArr[4]);//elements are not 4
+console.log(magazine);
+
+
+for(let i=0; i< myArr.length; i++){
+console.log(myArr[i]);
+}
+
+//Arrays in javascript create shallow copies
+
+/*
+SHALLOW COPY -- Properties share same references.
+DEEP COPY    -- Properties that do not share the same references.
+
+*/
+
+
+
+
+
+
+/*
+                                        VARIOUS METHODS OF ARRAYS
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+*/
+
